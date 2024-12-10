8abd956a21c73297668cbda8825aba08bcdeb6f2
logesh3027 <learningcode3027@gmail.com>
2024-12-10 11:17:07 +0530

    Add files via upload

---
+++ bookstore/src/main/java/com/example/demo/controller/Bookcontroller.java
@@ -0,0 +1,76 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Bookentity;
+import com.example.demo.repository.Bookrepository;
+
+@RestController
+public class Bookcontroller {
+@Autowired	
+	public Bookrepository arepo;
+@PostMapping("/addbook")
+public Bookentity addbook(@RequestBody Bookentity mov) {
+	try {
+		arepo.save(mov);
+		return mov;
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	return null;
+	
+}
+@PutMapping("/updatebook")
+public Bookentity updatebook(@RequestBody Bookentity mov) {
+	try{
+		arepo.save (mov);
+		return mov;
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	return null;
+}
+
+@GetMapping("/getbook")
+public List<Bookentity> getbook(){
+	try {
+		return arepo.findAll();
+		
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	return null;
+}
+
+
+
+@DeleteMapping("/deletebook/{bookid}")
+public String deletebook(@PathVariable int bookid) {
+	try {
+		Bookentity mov = arepo.getOne(bookid);
+		arepo.delete(mov);
+		return "Book has been removed";
+	}catch(Exception e) {
+		e.printStackTrace();
+		
+	}
+	return null;
+}
+}
+
+
+
+
+
+
+
